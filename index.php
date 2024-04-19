f7c2a8a0db6dd59c85f90446dbfe3b1000d32876
rupom <mohrajulrupom@gmail.com>
2024-04-19 21:58:08 +0600

    create table

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-[/**
+/**
  * Plugin Name: Database
  * Author: Rupom
  * Description: Plugin description
  * Version: 1.0
- */]
+ */
+
+ global $wpdb;
+ $table_name = $wpdb->prefix.'persons';
+ $sql = "CREATE TABLE {$table_name}(
+    id INT NOT NULL AUTO_INCREMENT,
+    p_name VARCHAR(250),
+    email VARCHAR(200),
+    PRIMARY KEY(id)
+ )" ;
+ require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+ dbDelta( $sql );
 ?>
